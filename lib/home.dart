72adb75b327d4a272ae2759a48bb31b1c6abecae
Muhammad Mannan <usmanmannan8@gmail.com>
2023-07-03 17:10:00 -0700

    started work on homepage

--- lib/home.dart
+++ lib/home.dart
@@ -1,13 +1,42 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
-  const HomePage({super.key});
+  HomePage({super.key});
+
+  final user = FirebaseAuth.instance.currentUser!;
+
+  //function to sign out user
+  void signUserOut() {
+    FirebaseAuth.instance.signOut();
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Text('Logged In'),
+    return Container(
+      decoration: BoxDecoration(
+        image: DecorationImage(
+          image: AssetImage("assets/Group2.png"),
+          fit: BoxFit.cover,
+        ),
+      ),
+      child: Scaffold(
+        backgroundColor: Colors.transparent,
+        appBar: AppBar(
+          elevation: 0,
+          backgroundColor: Colors.transparent,
+          actions: [
+            IconButton(
+                onPressed: signUserOut,
+                icon: Icon(
+                  Icons.logout,
+                  color: Color(0xFF234EF3),
+                ))
+          ],
+        ),
+        body: Center(
+          child: Text('Logged In As ' + user.email!),
+        ),
       ),
     );
   }
