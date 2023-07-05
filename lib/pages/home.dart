65ff0c8302f1e40c5912b2497eb2063d436d0e61
Muhammad Mannan <usmanmannan8@gmail.com>
2023-07-04 17:19:43 -0700

    started work on home page

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -14,28 +14,97 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      decoration: BoxDecoration(
+      decoration: const BoxDecoration(
         image: DecorationImage(
           image: AssetImage("assets/Group2.png"),
           fit: BoxFit.cover,
         ),
       ),
-      child: Scaffold(
-        backgroundColor: Colors.transparent,
-        appBar: AppBar(
-          elevation: 0,
+      child: SafeArea(
+        child: Scaffold(
           backgroundColor: Colors.transparent,
-          actions: [
-            IconButton(
-                onPressed: signUserOut,
-                icon: Icon(
-                  Icons.logout,
-                  color: Color(0xFF234EF3),
-                ))
-          ],
-        ),
-        body: Center(
-          child: Text('Logged In As ' + user.email!),
+          appBar: AppBar(
+            title: const Text(
+              'DayFlow',
+              style: TextStyle(
+                color: Color(0xFF234EF3),
+              ),
+            ),
+            elevation: 0,
+            backgroundColor: Colors.transparent,
+            actions: [
+              Padding(
+                padding: const EdgeInsets.only(right: 24.0),
+                child: IconButton(
+                    onPressed: signUserOut,
+                    icon: Icon(
+                      Icons.logout,
+                      color: Color(0xFF234EF3),
+                    )),
+              )
+            ],
+          ),
+          body: Container(
+            child: Column(
+              children: [
+                const Padding(
+                  padding: EdgeInsets.all(24),
+                  child: Text(
+                    'Welcome\nBack! 👋',
+                    style: TextStyle(
+                      fontSize: 60,
+                      fontWeight: FontWeight.bold,
+                      color: Color(0xFF234EF3),
+                    ),
+                  ),
+                ),
+                Center(
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 30.0),
+                    child: Container(
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(10),
+                        boxShadow: [
+                          BoxShadow(
+                            color: Color.fromARGB(255, 149, 166, 192)
+                                .withOpacity(0.25),
+                            spreadRadius: 0,
+                            blurRadius: 10,
+                            offset: Offset(
+                                0, 3), // changes the position of the shadow
+                          ),
+                        ],
+                      ),
+                      child: Padding(
+                        padding: const EdgeInsets.symmetric(
+                            vertical: 8.0, horizontal: 10),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Text(
+                              'Create an entry',
+                              style: TextStyle(fontSize: 20),
+                            ),
+                            Container(
+                              decoration: BoxDecoration(
+                                color: Color(0xFF234EF3),
+                                borderRadius: BorderRadius.circular(5),
+                              ),
+                              child: Icon(
+                                Icons.add,
+                                color: Colors.white,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
         ),
       ),
     );
