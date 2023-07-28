56f8fb98b95edf254545bf2e3f93ad7f77fb06f4
Muhammad Mannan <usmanmannan8@gmail.com>
2023-07-28 15:30:37 -0700

    rename pages for better organization

--- lib/pages/signin.dart
+++ lib/pages/signin.dart
@@ -1,5 +1,5 @@
 // ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
-import 'package:dayflow/pages/home.dart';
+import 'package:dayflow/pages/taskPage.dart';
 import 'package:dayflow/pages/register.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
